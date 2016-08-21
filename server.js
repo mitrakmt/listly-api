cd90ac57aaa025f63618437578e1a9ec37a63eca
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-08-21 13:31:48 -0700

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -59,17 +60,23 @@ app.delete('/todos/:id', function (request, response) {
 app.post('/todos', function (request, response) {
   var body = _.pick(request.body, 'description', 'completed');
 
-  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-    return response.status(400).send();
-  }
+  db.todo.create(body).then(function (todo) {
+    response.json(todo.toJSON());
+  }, function(error) {
+    response.status(400).send(error);
+  });
 
-  body.description = body.description.trim();
-
-  body.id = todoNextId++;
-
-  todos.push(body);
-
-  response.json(body);
+  // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+  //   return response.status(400).send();
+  // }
+  //
+  // body.description = body.description.trim();
+  //
+  // body.id = todoNextId++;
+  //
+  // todos.push(body);
+  //
+  // response.json(body);
 });
 
 // Update Todo
@@ -97,8 +104,10 @@ app.put('/todos/:id', function (request, response) {
 
   _.extend(matchedTodo, validAttributes);
   response.json(matchedTodo)
-})
+});
 
-app.listen(PORT, function () {
-  console.log("Express listening on port: " + PORT)
-})
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function () {
+    console.log("Express listening on port: " + PORT)
+  });
+});
