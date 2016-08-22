b98b1cb29503ce92eac2c3222db1d381d597acbf
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-08-22 13:36:40 -0700

    Add auth middleware and findByToken class method

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
 var bcrypt = require('bcryptjs');
+var middleware = require('./middleware.js')(db);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -15,7 +16,7 @@ app.get('/', function (request, response) {
   response.send('Todo API root')
 })
 
-app.get('/todos', function (request, response) {
+app.get('/todos', middleware.requireAuthentication, function (request, response) {
   var query = request.query;
   var where = {};
 
@@ -38,7 +39,7 @@ app.get('/todos', function (request, response) {
   });
 })
 
-app.get('/todos/:id', function (request, response) {
+app.get('/todos/:id', middleware.requireAuthentication, function (request, response) {
   var todoId = parseInt(request.params.id, 10);
 
   db.todo.findById(todoId).then(function(todo) {
@@ -53,7 +54,7 @@ app.get('/todos/:id', function (request, response) {
 });
 
 // Delete Todos
-app.delete('/todos/:id', function (request, response) {
+app.delete('/todos/:id', middleware.requireAuthentication, function (request, response) {
   var todoId = parseInt(request.params.id, 10);
 
   db.todo.destroy({
@@ -74,7 +75,7 @@ app.delete('/todos/:id', function (request, response) {
   });
 
 
-app.post('/todos', function (request, response) {
+app.post('/todos', middleware.requireAuthentication, function (request, response) {
   var body = _.pick(request.body, 'description', 'completed');
 
   db.todo.create(body).then(function (todo) {
@@ -85,7 +86,7 @@ app.post('/todos', function (request, response) {
 });
 
 // Update Todo
-app.put('/todos/:id', function (request, response) {
+app.put('/todos/:id', middleware.requireAuthentication, function (request, response) {
   var todoId = parseInt(request.params.id, 10);
   var body = _.pick(request.body, 'description', 'completed');
   var attributes = {};
